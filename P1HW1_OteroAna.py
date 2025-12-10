a48f0940118196ac53aa3064e55190a8bacd9bfb
Ana33411 <bhadbhabie.a@gmail.com>
2025-12-10 15:14:07 -0500

    P1HW1_OteroAna.py

---
+++ P1HW1_OteroAna.py
@@ -0,0 +1,11 @@
+print("-----Calculating Exponents----\n")
+base = int(input("Enter an integer as the base value:"))
+exponent = int(input("Enter an integer as the exponent:"))
+result = base ** exponent
+print(f"\n{base} raised to the power of {exponent} is {result} !!\n")
+print("-----Addition and Subtraction----\n")
+start = int(input("Enter a starting integer:"))
+to_add = int(input("Enter an integer to add:"))
+to_subtract = int(input("Enter an integer to subtract:"))
+final_result = start + to_add - to_subtract
+print(f"\n{start} + {to_add} - {to_subtract} is equal to {final_result}")
